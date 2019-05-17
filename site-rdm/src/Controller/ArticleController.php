0981be022fbe413c5b3aa6c5766feb5cc278a20e
gwadada <koulou91@wanadoo.fr>
2019-05-17 21:53:38 +0200

    Upload image article

--- site-rdm/src/Controller/ArticleController.php
+++ site-rdm/src/Controller/ArticleController.php
@@ -1,10 +1,20 @@
 <?php
 
-
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
-
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index(ArticleRepository $articleRepository)
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll()
+        ]);
+    }
 }
--- site-rdm/src/Entity/Article.php
+++ site-rdm/src/Entity/Article.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ * @Vich\Uploadable
  */
 class Article extends AbstractController
 {
@@ -16,8 +19,6 @@ class Article extends AbstractController
      * @ORM\Column(type="integer")
      */
     private $id;
-
-
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -33,6 +34,27 @@ class Article extends AbstractController
      * @ORM\Column(type="integer", nullable=true)
      */
     private $likes;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image_name;
+
+    /**
+     * @Vich\UploadableField(mapping="article_image", fileNameProperty="image_name")
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function __construct()
+    {
+        $this->updatedAt = new \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -71,6 +93,53 @@ class Article extends AbstractController
     public function setLikes(?int $likes): self
     {
         $this->likes = $likes;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->image_name;
+    }
+
+    public function setImageName(?string $image_name): self
+    {
+        $this->image_name = $image_name;
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param File|null $imageFile
+     * @throws \Exception
+     */
+    public function setImageFile(?File $imageFile): void
+    {
+        $this->imageFile = $imageFile;
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
